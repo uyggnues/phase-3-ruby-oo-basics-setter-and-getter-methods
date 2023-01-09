b2fabbec6e5e8352aade2731322a430d3d61c076
Seunggyu <seunggyul14@icloud.com>
2023-01-08 19:19:08 -0800

    commit tree 87b7526133d80c2b5e6dbce484ed87871120273d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+    attr_reader :name, :breed
 
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
+    attr_reader :name, :job
 
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
 end
